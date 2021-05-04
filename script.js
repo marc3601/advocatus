602175f82e45a453de159dbad33035ba50644b84
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-05-04 22:35:00 +0200

    Work time 4.5 h

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const bu
+const navLinks = document.querySelectorAll('.nav_button');
+const handleHighlight = (item) => {
+  item.classList.add('nav_button_active');
+};
+const removeHighlight = () => {
+  navLinks.forEach((el) => {
+    el.classList.remove('nav_button_active');
+  });
+};
+navLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    removeHighlight();
+    handleHighlight(item);
+  });
+});
